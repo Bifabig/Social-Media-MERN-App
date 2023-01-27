7d1d5a9319f7dd75fd08dcfa6f4a9e68d52b380c
Bifabig <biftubig@gmail.com>
2023-01-27 17:48:26 +0300

    Navbar for desktop done

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import authReducer from "./state";
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import {
   persistStore,
@@ -16,7 +16,7 @@ import {
 } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { PersistGate } from "redux-persist/integration/react";
-import persistReducer from "redux-persist/es/persistReducer";
+// import persistReducer from "redux-persist/es/persistReducer";
 
 const persistConfig = { key: "root", storage, version: 1 };
 const persistReducer = persistReducer(persistConfig, authReducer);
